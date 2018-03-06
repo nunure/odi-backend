69569cd9d14ab676acb6dd7fa69920427bb4ca53
Mathieu RETY <rety.mathieu@gmail.com>
2018-03-06 18:52:41 +0100

    :recycle: Removing usless declaration

--- app.js
+++ app.js
@@ -2,23 +2,19 @@ const express = require('express');
 const winston = require('winston');
 
 const configExpress = require('./routes/config/express');
-const index = require('./routes/index');
-const users = require('./routes/users');
-const home = require('./routes/home/');
-const questions = require('./routes/questions/');
 
 winston.cli();
 winston.info('Server process starting');
 
 const app = express();
 
+const home = require('./routes/home/');
+const questions = require('./routes/questions/');
 const errorHandler = require('./routes/middleware/error-handler');
 const notFound = require('./routes/middleware/not-found');
 
 // Déclaration des routes
 configExpress(app);
-app.use('/', index);
-app.use('/users', users);
 app.use('/home', home.router);
 app.use('/questions', questions.router);
 app.use(errorHandler());
