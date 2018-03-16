45518f8b4f7fb6f86961b36bb222065034884d9a
Mathieu RETY <rety.mathieu@gmail.com>
2018-03-16 09:45:56 +0100

    :wrench: Add Access-Control-Allow-Origin for front purpose

--- routes/config/express.js
+++ routes/config/express.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
   app.use(morgan('dev', { stream: { write: message => winston.info(message) } }));
   app.use(bodyParser.json());
   app.use(logger('dev'));
