75357a7133cd1ae57d0b7e803ff2f9bb9c60e396
Mathieu RETY <rety.mathieu@gmail.com>
2018-03-16 17:51:27 +0100

    :card_file_box: Changing db Schema

--- routes/questions/questions.js
+++ routes/questions/questions.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 
+// A éclaircir, pourquoi est-ce que si on met n'imp ici
+// on récupère quand même ce qu'il faut dans le front ?
+// Est-ce qu'on doit préciser à chaque fois les cham requis ?
+// Si oui à préciser en base égallement ?
 const QuestionsSchema = new mongoose.Schema({
-  title: String,
-  answers: [String],
+  type: String,
+  inputType: String,
+  label: String,
+  model: String,
+  maxLength: Number,
+  placeholder: String,
+  page: Number,
+  required: Boolean,
+  answers: [{
+    name: String,
+    value: Number,
+  }],
 });
 
 module.exports = mongoose.model('Questions', QuestionsSchema);
