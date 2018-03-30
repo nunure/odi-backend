7a29898df604ed61c863878ba36a6d5ae2fd4295
Mathieu RETY <rety.mathieu@gmail.com>
2018-03-30 14:48:55 +0200

    :card_file_box: Changing base format

--- routes/questions/questions.js
+++ routes/questions/questions.js
@@ -5,18 +5,22 @@ const mongoose = require('mongoose');
 // Est-ce qu'on doit préciser à chaque fois les cham requis ?
 // Si oui à préciser en base égallement ?
 const QuestionsSchema = new mongoose.Schema({
-  type: String,
-  inputType: String,
-  label: String,
-  model: String,
-  maxLength: Number,
-  placeholder: String,
   page: Number,
-  required: Boolean,
-  answers: [{
-    name: String,
-    value: Number,
-  }],
+  template: {
+    fields: [{
+      type: String,
+      inputType: String,
+      label: String,
+      model: String,
+      maxLength: Number,
+      placeholder: String,
+      required: Boolean,
+      values: [{
+        name: String,
+        value: Number,
+      }],
+    }],
+  },
 });
 
 module.exports = mongoose.model('Questions', QuestionsSchema);
