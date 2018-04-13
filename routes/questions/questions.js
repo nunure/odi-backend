d6bef60f9f256c30d03e7c5ad5e0324784545eac
Mathieu RETY <rety.mathieu@gmail.com>
2018-04-13 09:37:16 +0200

    :bug: Fixes #1 conflict with type filed

--- routes/questions/questions.js
+++ routes/questions/questions.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
-// A éclaircir, pourquoi est-ce que si on met n'imp ici
-// on récupère quand même ce qu'il faut dans le front ?
-// Est-ce qu'on doit préciser à chaque fois les cham requis ?
-// Si oui à préciser en base égallement ?
 const QuestionsSchema = new mongoose.Schema({
   page: Number,
   template: {
-    fields: [{
-      type: String,
-      inputType: String,
-      label: String,
-      model: String,
-      maxLength: Number,
-      placeholder: String,
-      required: Boolean,
-      values: [{
-        name: String,
-        value: Number,
-      }],
-    }],
+    fields: [
+      {
+        inputType: String,
+        label: String,
+        maxLength: Number,
+        model: String,
+        placeholder: String,
+        required: Boolean,
+        type: { type: String, default: undefined },
+        values: [
+          {
+            name: String,
+            value: Number,
+          },
+        ],
+      },
+    ],
   },
 });
 
