439ecfdaf787edd1a65bd23a14d7577a1a7232e8
Mathieu RETY <rety.mathieu@gmail.com>
2018-04-19 12:21:36 +0200

    :wrench: Allow GET, PUT, POST and DELETE requests

--- routes/config/express.js
+++ routes/config/express.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
   });
   app.use(morgan('dev', { stream: { write: message => winston.info(message) } }));
